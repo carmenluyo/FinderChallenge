4a2078565cc033b2121710760468adfb77c68f72
Carmen Luyo <c.luyorosales@gmail.com>
2018-01-10 14:40:09 -0500

    add search automplete fixed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,13 +4,37 @@
 
 searchForm();
 
-var JSON_FILE = '/books_schema.json';
-console.log(JSON_FILE);
-$.each(JSON_FILE, function(i, v) {
-    if (v.name == "Peter") {
-        // found it...
-        alert(v.age);
-        return false; // stops the loop
+
+(function(window, document, $, undefined) {
+
+    $(document).on('keyup', '#myinput', function (e) {
+        if(e.keyCode == 13) {
+            searchData();
+        }
+    });
+
+    $(document).on('click', '.btn-src', function () {
+        searchData();
+    });
+
+    function searchData(){
+        var word = $('#myinput').val();
+        $('#gr-items-books').html('');
+        $.getJSON( "/books-schema.json", function( result) {
+            $.each( result.data, function( key, item ) {
+                var name = item.title;
+                if (name.toLowerCase().indexOf(word) >= 0)  { 
+                    var html = '<article style="display: block;">';
+                    html += '<img src="'+item.image+'">';
+					html += '<h2>'+item.title+'</h2>';
+					html += '<p>'+item.teaser+'</p></article>';
+                    $('#gr-items-books').append(html);
+                }
+            });
+        });
+        
+         
     }
 
-});
+
+})(window, document, jQuery);
